c626f2d58743e0edc6f730965d1ed6b004511444
Nagy László Levente <nagylacika2002@gmail.com>
2023-08-11 23:19:19 +0200

    Changed adding filed to react style

--- src/App.js
+++ src/App.js
@@ -1,72 +1,37 @@
 import './App.css';
 import Header from "./components/Header";
 import InputFields from "./components/InputFields";
+import {useState} from "react";
+import AddFields from "./components/AddFields";
 
+
+let id = 0;
 
 function App() {
-    let id = 0;
-    let todoArray = [];
-    //const [todoArray, setTodoArray] = useState([]);
+    //let todoArray = [];
+    const [todoArray, setTodoArray] = useState([]);
 
-
-    const insertTodoInOrder = (todoElement) => {
-        /*setTodoArray((prevState) => {
-            return [...prevState, todoElement];
-        });*/
-        todoArray.push(todoElement);
-        todoArray.sort((a, b) => {
-            return new Date(a.date) - new Date(b.date);
-        });
-
-        const todoField = document.querySelector('.todo-fields');
-        todoField.innerHTML = "";
-        for (let i = 0; i < todoArray.length; i++) {
-            const todoF = document.createElement("div");
-            todoF.classList.add("flexx");
-            todoF.innerHTML = `<div class="inpText" data-id=${todoArray[i].id}>${todoArray[i].text}</div>
-                    <div class="inpDate">${todoArray[i].date}</div>
-                    <button class="remove-todo">Remove</button>`;
-            todoField.appendChild(todoF);
-        }
-        console.log(todoField);
-
-        const rmButtons = document.querySelectorAll(".remove-todo");
-        rmButtons.forEach(item => item.addEventListener('click', event => {
-            const containerDiv = event.target.parentElement;
-            for (let i = 0; i < todoArray.length; i++) {
-                if (todoArray[i].id === parseInt(containerDiv.children[0].getAttribute("data-id"))) {
-                    todoArray.splice(i, 1);
-                    break;
-                }
-            }
-            containerDiv.remove();
-        }));
-
-
-    }
     const add = () => {
-        //<AddFields insertTodoInOrder={insertTodoInOrder()} />
         const todoText = document.querySelector("#todoText").value;
         const todoDate = document.querySelector("#todoDate").value;
-        /*(
-            <div className="flexx">
-                <div className="inpText" data-id={id}>${todoText}</div>
-                <div className="inpDate">${todoDate}</div>
-                <button className="remove-todo">Remove</button>
-            </div>
-        )*/
         const data = {
             text: todoText,
             date: todoDate,
             id: id++
         }
-        insertTodoInOrder(data);
+        console.log(data);
+        setTodoArray((prevState) => {
+            let a = [...prevState, data];
+            return a.sort((a, b) => {
+                return new Date(a.date) - new Date(b.date);
+            });
+        });
     }
     return (
         <div className="App">
-            <Header/>
+            <Header count={todoArray.length}/>
             <InputFields add={add}/>
-            <div className="todo-fields"></div>
+            <AddFields tdArray={todoArray}/>
         </div>
     );
 }
--- src/components/AddFields.js
+++ src/components/AddFields.js
@@ -1,15 +1,37 @@
+import {useRef} from "react";
+
 export default function AddFields(props) {
-    const todoText = document.querySelector("#todoText").value;
-    const todoDate = document.querySelector("#todoDate").value;
-    const a = (
-        <div className="flexx">
-            <div className="inpText" data-id={props.id}>${todoText}</div>
-            <div className="inpDate">${todoDate}</div>
-            <button className="remove-todo">Remove</button>
+
+    const alma = (tod) => {
+        return (
+            <div className="flexx">
+                <div className="inpText" data-id={tod.id}>{tod.text}</div>
+                <div className="inpDate">{tod.date}</div>
+                <button className="remove-todo">Remove</button>
+            </div>
+        )
+    }
+    /*console.log(todoField);
+
+    const rmButtons = document.querySelectorAll(".remove-todo");
+    rmButtons.forEach(item => item.addEventListener('click', event => {
+        const containerDiv = event.target.parentElement;
+        for (let i = 0; i < todoArray.length; i++) {
+            if (todoArray[i].id === parseInt(containerDiv.children[0].getAttribute("data-id"))) {
+                todoArray.splice(i, 1);
+                break;
+            }
+        }
+        containerDiv.remove();
+    }));*/
+
+    return (
+        <div className="todo-fields">
+            {props.tdArray.map((todo) => {
+                return (
+                    <div key={todo.id}>{alma(todo)}</div>
+                );
+            })}
         </div>
     )
-    props.insertTodoInOrder(a);
-
-    return a;
-
 }
